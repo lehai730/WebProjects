53f60744e36c6683201167a61b7d2ed4f2fa1362
lehai730 <lehai730@gmail.com>
2019-11-11 09:28:12 +1100

    completed number guesser

--- 3-Number-Guesser/script.js
+++ 3-Number-Guesser/script.js
@@ -3,4 +3,29 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random()*10);
+};
 
+const compareGuesses = (human, computer, target) => {
+    if (Math.abs(target-human) === Math.abs(target-computer)) {
+        return true;
+    } else if (Math.abs(target - human) > Math.abs(target - computer)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    } else {
+        console.log('updateScore have wrong input');
+    }
+}
+
+const advanceRound = () => currentRoundNumber++;
+console.log(generateTarget());
